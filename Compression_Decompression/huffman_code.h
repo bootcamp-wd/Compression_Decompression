a4b852e04770edda0f9b464667edbbc41ddd95d5
b.lock <lbella9526@gmail.com>
2024-08-15 16:03:26 +0300

    some changes before merging

--- Compression_Decompression/huffman_code.h
+++ Compression_Decompression/huffman_code.h
@@ -1,19 +1,22 @@
 #ifndef HUFFMAN_CODE_H
 #define HUFFMAN_CODE_H
 
-#include "general_defines.c"
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct huffman_decode_node {
-	struct huffman_decode_node* left;
-	struct huffman_decode_node* right;
-	char byte;
+
+	unsigned char by_ascii;
+	unsigned char left;
+	unsigned char right;
 }Huffman_decode_node;
 
-unsigned char find_ascii_in_tree(unsigned char** input_pointer, Huffman_decode_node root,
+int rescu_metadata(unsigned char* input_buffer, Huffman_decode_node* root);
+
+void huffman_decode(unsigned char* input_buffer, int* input_size, unsigned char* output_buffer);
+
+unsigned char find_ascii_in_tree(unsigned char* input_pointer, Huffman_decode_node* root,
 	int* bits_index);
-
-void huffman_decode(unsigned char* data_to_decompress,
-	unsigned char** decompressed_data, int* input_size, Huffman_decode_node root);
-
 
 
 #endif // !HUFFMAN_CODE_H
