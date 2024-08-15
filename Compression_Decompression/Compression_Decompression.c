1feca14c6edada6e6df2cafc4fec420262707591
yaelnNoifeld <yael90669@gmail.com>
2024-08-15 16:49:21 +0300

    changes in the main file

--- Compression_Decompression/Compression_Decompression.c
+++ Compression_Decompression/Compression_Decompression.c
@@ -15,30 +15,7 @@ int main()
     printf("Simulating compression...\n");
     process_file(original_file, compressed_file, 1);
 
-<<<<<<< HEAD
-	lz77_encode(input_data, input_size, output_data, &output_size, 12, 12);
-
-	for (int i = 0; i < output_size; i+=2)
-	{
-		printf("%hi", output_data[i]);
-		i += 2;
-		printf("%hi", output_data[i]);
-		i += 2;
-		printf("%c", output_data[i]);
-	}
-	printf("\n");
-	unsigned char* output_buffer_lz_decode = (unsigned char*)malloc(6 * sizeof(char));
-
-	lz77_decode(output_data, &output_size, output_buffer_lz_decode);
-	for (int i = 0; i < strlen(input_data); i++)
-	{
-		printf("%c", output_buffer_lz_decode[i]);
-	}
-
-	free(output_data);
-=======
     // Simulate decompression
     printf("\nSimulating decompression...\n");
     process_file(compressed_file, decompressed_file, 0);
->>>>>>> 5e74596b3d23be3d236b91c9c67957fecbee3416
 }
