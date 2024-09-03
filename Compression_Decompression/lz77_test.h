63a7ac3f519ceca412f3ba4dd7f9f40fefb390a7
nami-p <namipr3@gmail.com>
2024-09-03 16:22:29 +0300

    ignore the lz77 tests - make as comment

--- Compression_Decompression/lz77_test.h
+++ Compression_Decompression/lz77_test.h
@@ -1,28 +1,28 @@
-#ifndef LZ77_H
-#define LZ77_H
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include "lz77.h"
-#include "general_define.h"
-
-void lz77_testing();
-
-void lz77_test_treatment(U_08* input_buffer, U_32 input_size, U_08* compressed_buffer, U_32 compressed_size,
-	const U_32 compress_level, U_08* decompressed_buffer);
-
-void lz77_test_regular_size(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
-	const U_32 compress_level, U_08* decompressed_buffer);
-
-	void lz77_test_identical_characters(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
-		const U_32 compress_level, U_08* decompressed_buffer);
-
-void lz77_test_empty(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
-	const U_32 compress_level, U_08* decompressed_buffer);
-
-void lz77_test_many_repeat(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
-	const U_32 compress_level, U_08* decompressed_buffer);
-
-#endif // !LZ77_H
+//#ifndef LZ77_H
+//#define LZ77_H
+//
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <time.h>
+//#include "lz77.h"
+//#include "general_define.h"
+//
+//void lz77_testing();
+//
+//void lz77_test_treatment(U_08* input_buffer, U_32 input_size, U_08* compressed_buffer, U_32 compressed_size,
+//	const U_32 compress_level, U_08* decompressed_buffer);
+//
+//void lz77_test_regular_size(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
+//	const U_32 compress_level, U_08* decompressed_buffer);
+//
+//	void lz77_test_identical_characters(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
+//		const U_32 compress_level, U_08* decompressed_buffer);
+//
+//void lz77_test_empty(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
+//	const U_32 compress_level, U_08* decompressed_buffer);
+//
+//void lz77_test_many_repeat(U_08* input_buffer, U_32* input_size, U_08* compressed_buffer, U_32* compressed_size,
+//	const U_32 compress_level, U_08* decompressed_buffer);
+//
+//#endif // !LZ77_H
