2fcb51ccbf539e3c0abe1073b6b0a7bfa0106b1b
nami-p <namipr3@gmail.com>
2024-09-04 16:40:41 +0300

    try again push changes didnt update

--- Compression_Decompression/compressor_decompressor.c
+++ Compression_Decompression/compressor_decompressor.c
@@ -9,10 +9,10 @@
 #define BUFFER_SIZE 4096
 
 // Function to process the file (either compress or decompress)
-void process_file(const U_08* input_path, const U_08* output_path,U_32 compress_level ,U_32 compress)
+void process_file(const U_08* input_path, const U_08* output_path,const U_32 compress_level ,const U_32 compress)
 {
-    unsigned char* input_buffer_p = NULL, * compressed_data_buffer_p = NULL;
-    S_32 file_size = 0, compressed_size = 0;
+    U_08* input_buffer_p = NULL, * compressed_data_buffer_p = NULL;
+    S_32 file_size = 0, compressed_buffer_size = 0;
     FILE* metadata_file = NULL;
     char metadata_path[BUFFER_SIZE];
     char extension[BUFFER_SIZE] = { 0 };
@@ -44,7 +44,7 @@ void process_file(const U_08* input_path, const U_08* output_path,U_32 compress_
         fprintf(metadata_file, "%s", extension);
         fclose(metadata_file);
 
-        compress_data(input_buffer_p, file_size, &compressed_data_buffer_p, &compressed_size,compress_level);
+        compress_data(input_buffer_p, file_size, &compressed_data_buffer_p, &compressed_buffer_size,compress_level);
     }
     else {
         // Read extension from metadata file
@@ -61,7 +61,7 @@ void process_file(const U_08* input_path, const U_08* output_path,U_32 compress_
         // Append extension to output path
         snprintf(metadata_path, sizeof(metadata_path), "%s.%s", output_path, extension);
 
-        decompress_data(input_buffer_p, file_size, &compressed_data_buffer_p, &compressed_size);
+        decompress_data(input_buffer_p, file_size, &compressed_data_buffer_p, &compressed_buffer_size);
 
         // Update the output path with the constructed file name
         output_path = metadata_path;
@@ -71,39 +71,39 @@ void process_file(const U_08* input_path, const U_08* output_path,U_32 compress_
     free(input_buffer_p);
 
     // Write the compressed_data_buffer_p data to the output file
-    write_file(output_path, compressed_data_buffer_p, compressed_size);
+    write_file(output_path, compressed_data_buffer_p, compressed_buffer_size);
 
     // Free the compressed_data_buffer_p buffer
     free(compressed_data_buffer_p);
 
-    printf("File compressed_data_buffer_p successfully. Input size: %d bytes, Output size: %d bytes\n", file_size, compressed_size);
+    printf("File compressed_data_buffer_p successfully. Input size: %d bytes, Output size: %d bytes\n", file_size, compressed_buffer_size);
 }
 
-void compress_data(const U_08* input_buffer, U_32 input_size,U_08* output_buffer, U_32* output_size,const U_32 compress_level)
+void compress_data(const U_08* input_buffer_p, U_32 input_size,U_08* output_buffer_p, U_32* output_size,const U_32 compress_level)
 {
-    output_buffer = (unsigned char*)malloc((input_size* get_size_of_encoded_sequence_struct() +sizeof(int)));//alocat the memory 
-    *output_buffer = input_size;//save the size of the data
-    if (output_buffer == NULL)
+    output_buffer_p = (unsigned char*)malloc((input_size* get_size_of_encoded_sequence_struct() +sizeof(int)));//alocat the memory 
+    *output_buffer_p = input_size;//save the size of the data
+    if (output_buffer_p == NULL)
     {
         perror("Memory allocation failed in simulate_compress_data");
         exit(1);
     }
-   lz77_encode(input_buffer, input_size, output_buffer+sizeof(int), output_size, compress_level,0);
-   huffman_encode(output_buffer+sizeof(int), output_buffer + sizeof(int), *output_size, output_size);
+   lz77_encode(input_buffer_p, input_size, output_buffer_p +sizeof(int), output_size, compress_level,0);
+   huffman_encode(output_buffer_p +sizeof(int), output_buffer_p + sizeof(int), *output_size, output_size);
 
-   realloc(output_buffer, (output_size + sizeof(int)));//decreas the memory size after finish the comression progress 
+   realloc(output_buffer_p, (output_size + sizeof(int)));//decreas the memory size after finish the comression progress 
 }
 
-void decompress_data(const U_08* input_buffer, U_32 input_size, U_08* output_buffer, U_32* output_size)
+void decompress_data(const U_08* input_buffer_p, U_32 input_size, U_08* output_buffer_p, U_32* output_size)
 {
-    *output_size = input_buffer;
-    input_buffer += sizeof(int);
-    *output_buffer = (unsigned char*)malloc(output_size);
-    if (*output_buffer == NULL)
+    *output_size = input_buffer_p;
+    input_buffer_p += sizeof(int);
+    *output_buffer_p = (unsigned char*)malloc(output_size);
+    if (*output_buffer_p == NULL)
     {
         perror("Memory allocation failed in simulate_decompress_data");
         exit(1);
     }
-    huffman_decode(input_buffer,input_size,output_buffer,output_size);
-    lz77_decode(output_buffer,output_size,output_buffer);
+    huffman_decode(input_buffer_p,input_size,output_buffer_p,output_size);
+    lz77_decode(output_buffer_p,output_size,output_buffer_p);
 }
