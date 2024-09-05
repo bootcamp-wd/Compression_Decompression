dd4e4508ee573720632e29e8d982ab5713ea1f9e
b.lock <lbella9526@gmail.com>
2024-09-05 11:50:05 +0300

    fix after code review

--- Compression_Decompression/huffman_code.h
+++ Compression_Decompression/huffman_code.h
@@ -41,6 +41,6 @@ void huffman_free_tree(Huffman_node_t* nodes, U_32 last_index);
 Huffman_decode_node* rescu_metadata(U_08* input_buffer, U_32* tree_length);
 void huffman_decode(U_08* input_buffer, U_32* input_size, U_08* output_buffer);
 U_08 find_ascii_in_tree(U_08** input_pointer, Huffman_decode_node* root, U_32* bits_index, U_32 tree_length);
-void find_ascii_last_byte(U_08* ninput_pointer, Huffman_decode_node* root, U_32 bites_index, U_08* output_pointer, int tree_length);
+void find_ascii_last_byte(U_08* input_pointer, Huffman_decode_node* root, U_32 bites_index, U_08* output_pointer, int tree_length);
 
 #endif // !HUFFMAN_CODE_H
