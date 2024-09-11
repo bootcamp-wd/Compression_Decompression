d747ba3fd3d815036a32fc178488bf1b2075c63c
b.lock <lbella9526@gmail.com>
2024-09-11 16:42:42 +0300

    add the content to the tests files

---
+++ Testing_Compression_Decompression/file_test.c
@@ -0,0 +1,29 @@
+
+#include "file_test.h"
+
+void files_test()
+{
+	FILE* test_file;
+	U_08* buffer = "data to tests";
+	U_32 buffer_size = strlen(buffer);
+	write_test("test_file", buffer, buffer_size);
+	read_test("test_file", buffer, buffer_size);
+}
+
+void read_test(const U_08* file_path, const U_08* exepted_buffer, U_32 buffer_size)
+{
+	U_32 res_buffer_size;
+	U_08* res_buffer = read_file(file_path, &res_buffer_size);
+
+	ASSERT_EQUAL(res_buffer_size, buffer_size, "Worng file size");
+
+	ASSERT(strcmp(res_buffer, exepted_buffer), "The data is not written and read well from the file");
+
+}
+
+void write_test(const U_08* file_path, const U_08* buffer, U_32 buffer_size)
+{
+	write_file(file_path, buffer, buffer_size);
+}
+
+
--- Testing_Compression_Decompression/file_test.h
+++ Testing_Compression_Decompression/file_test.h
@@ -1,1 +1,43 @@
-#pragma once
+
+#ifndef TEST_FILE_H
+#define TEST_FILE_H
+
+#include "lz77_test.h"
+#include "../Compression_Decompression/general_define.h"
+#include "../Compression_Decompression/file_managment.h"
+
+#define ASSERT(condition, message) \
+    do { \
+        if (!(condition)) { \
+            printf("ASSERTION FAILED: %s\n", message); \
+            printf("File: %s, Line: %d\n", __FILE__, __LINE__); \
+            return; \
+        } \
+        else \
+        { \
+            printf("TEST PASSED\n"); \
+        } \
+    } while (0)
+
+#define ASSERT_EQUAL(expected, actual, message) \
+    do { \
+        if ((expected) != (actual)) { \
+            printf("ASSERTION FAILED: %s\n", message); \
+            printf("Expected: %d, Actual: %d\n", (int)(expected), (int)(actual)); \
+            printf("File: %s, Line: %d\n", __FILE__, __LINE__); \
+            return; \
+        } \
+         else \
+        { \
+            printf("TEST PASSED\n"); \
+        } \
+    } while (0)
+
+
+void files_test();
+void read_test(const U_08* file_path, const U_08* exepted_buffer, U_32 file_size);
+void write_test(const U_08* file_path, const U_08* buffer, U_32 file_size);
+
+
+
+#endif 
